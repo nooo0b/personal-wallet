e6a22b9c506f17a5c76a2952cd791f62ef38d604
nooo0b <110136042+nooo0b@users.noreply.github.com>
2024-05-17 03:18:28 +0600

    check valid number

--- js/deposit.js
+++ js/deposit.js
@@ -1,6 +1,10 @@
 document.getElementById('btn-deposit').addEventListener('click', function(){
     const depositAmount = document.getElementById('deposit-amount');
     const deposit = parseFloat(depositAmount.value) ;
+    if (isNaN(deposit)) {
+        alert('Please Provide A Valid Number');
+        return;
+    }
 
     const totalDeposit = document.getElementById('total-deposit');
     const currentDeposit = parseFloat(totalDeposit.innerText);
