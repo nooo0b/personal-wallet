711e4b381ab7836fb0c6fdcdd8d997efbba7fd6d
nooo0b <110136042+nooo0b@users.noreply.github.com>
2024-06-28 18:16:09 +0600

    Update deposit.js

--- js/deposit.js
+++ js/deposit.js
@@ -1,6 +1,7 @@
 document.getElementById('btn-deposit').addEventListener('click', function(){
     const depositAmount = document.getElementById('deposit-amount');
     const deposit = parseFloat(depositAmount.value) ;
+    depositAmount.value = '';
     if (isNaN(deposit) || deposit < 0) {
         alert('Please Provide A Valid Number');
         return;
@@ -15,6 +16,4 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
     const currentBalance = parseFloat(balance.innerText);
 
     balance.innerText = currentBalance + deposit;
-
-    depositAmount.value = '';
 })
